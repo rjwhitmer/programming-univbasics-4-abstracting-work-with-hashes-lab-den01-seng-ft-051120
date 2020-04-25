d6be108f987d5d663ee9639cb6e68459ada54776
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 21:42:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  new_key = key
-  hash = {
-    :new_key => value
-  }
+  hash = Hash.new(key, value)
 end
 
 def read_from_hash(hash, key)
