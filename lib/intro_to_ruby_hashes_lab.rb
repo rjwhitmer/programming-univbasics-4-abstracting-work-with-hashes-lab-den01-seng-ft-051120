8637018d818891b03a3ab458899d6fd32fc256a4
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 21:46:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = Hash.new
-  
-  hash = [key, value]
+  { key => value }
   
 end
 
